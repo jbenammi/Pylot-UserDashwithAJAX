dd705ea19e6d077901399463a466ae6f906c11f1
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-05-15 21:02:45 -0700

    this is my commit of user Dashboard with ajax

---
+++ app/controllers/Dashboards.py
@@ -0,0 +1,139 @@
+from system.core.controller import *
+
+class Dashboards(Controller):
+    def __init__(self, action):
+        super(Dashboards, self).__init__(action)
+
+        self.load_model('Dashboard')
+        self.db = self._app.db
+
+    def index(self):
+         return self.load_view('index.html')
+
+    def welcome(self):
+        return self.load_view('/partials/welcome.html')
+
+    def view_register(self, errors=None):
+        if errors != None:
+            flash(errors)
+            return self.load_view('/partials/register.html')
+        else:
+            return self.load_view('/partials/register.html')
+
+    def view_login(self, errors=None):
+        if errors != None:
+            flash(errors)
+        return self.load_view('/partials/login.html')
+
+    def view_dashboard(self):
+        user_info = self.models['Dashboard'].get_all_users()
+        if session['logged_info']['access'] == 1:
+            return self.load_view('/partials/admin_dash.html', users = user_info)
+        elif session['logged_info']['access'] == 0:
+            return self.load_view('/partials/user_dash.html', users = user_info)
+
+    def login_nav(self):
+        return self.load_view('/partials/navlinks_login.html')
+
+    def loggedin_nav(self):
+        if session['logged_info']['access'] == 1:
+            return self.load_view('/partials/navlinks_admin.html')
+        return self.load_view('/partials/navlinks_user.html')        
+    
+    def register(self):
+        reg_result = self.models['Dashboard'].register_user(request.form)
+        if reg_result == 'registered':
+            session['registered'] = 'Thank you for Registering. Please Log in'
+            return redirect('/v_login')
+        else:
+            return self.view_register(reg_result)
+
+    def reg_new_user(self):
+        reg_result = self.models['Dashboard'].register_user(request.form)
+        if reg_result == 'registered':
+            return redirect('/view_dash')
+        else:
+            return self.new_user(reg_result)
+
+    def new_user(self, errors = None):
+        if errors != None:
+            flash(errors)
+            return self.load_view('/partials/new_user.html')
+        else:
+            return self.load_view('/partials/new_user.html')
+
+    def view_admin_edit(self, id):
+        user_info = self.models['Dashboard'].get_user(id)
+        return self.load_view('/partials/edit_user.html', user = user_info[0])
+
+    def view_profile(self, id):
+        user_info = self.models['Dashboard'].get_user(id)
+        return self.load_view('/partials/profile.html', user = user_info[0])
+
+    def edit_user_info(self):
+        user = self.models['Dashboard'].update_user_info(request.form)
+        return self.view_profile(user)
+
+    def admin_edit_user_info(self):
+        user = self.models['Dashboard'].admin_update_user_info(request.form)
+        return self.view_dashboard()
+
+    def edit_user_desc(self):
+        user = self.models['Dashboard'].update_user_description(request.form)
+        return self.view_profile(user)        
+
+    def edit_user_pass(self):
+        user = self.models['Dashboard'].update_user_password(request.form)
+        if 'id' in user:
+            flash({'updated': "Password Updated"})
+            return self.view_profile(user['id'])
+        else:
+            flash(user)
+            return self.view_profile(user['user_id'])
+
+    def view_remove_user(self, id):
+        print id
+        print "*" * 50
+        user = self.models['Dashboard'].get_user(id)
+        return self.load_view('/partials/remove_user.html', user = user[0])
+
+    def delete_user(self, id):
+        self.models['Dashboard'].remove_user(id)
+        return redirect('/view_dash')       
+
+    def admin_edit_user_pass(self):
+        user = self.models['Dashboard'].update_user_password(request.form)
+        if 'id' in user:
+            flash({'updated': "Password Updated"})
+            return self.view_admin_edit(user['id'])
+        else:
+            flash(user)
+            return self.view_admin_edit(user['user_id'])
+
+    def view_wall(self, id):
+        wall_info = self.models['Dashboard'].get_user(id)
+        messages = self.models['Dashboard'].get_messages(id)
+        comments = self.models['Dashboard'].get_comments(id)
+        return self.load_view('/partials/user_wall.html', messages = messages, comments = comments, wall_info = wall_info[0])
+
+    def add_messages(self):
+        wall_id = self.models['Dashboard'].add_message(request.form)
+        return self.view_wall(wall_id)
+
+    def add_comments(self, id):
+        wall_id = self.models['Dashboard'].add_comment(request.form, id)
+        return self.view_wall(wall_id)
+
+    def login(self):
+        if 'registered' in session:
+            del session['registered']
+        log_result = self.models['Dashboard'].login_user(request.form)
+        if 'logged_info' in log_result:
+            session['logged_info'] = log_result['logged_info']
+            return self.view_dashboard()
+        else:
+            return self.view_login(log_result)
+
+    def logout(self):
+        session.clear()
+        return redirect('/')
---
+++ app/models/Dashboard.py
@@ -0,0 +1,168 @@
+from system.core.model import Model
+import re
+EMAILREGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+class Dashboard(Model):
+    def __init__(self):
+        super(Dashboard, self).__init__()
+
+    def register_user(self, reginfo):
+        errors = {}
+        if len(reginfo['fname']) < 2:
+            errors.update({'fname': 'The first name field must be at least two characters'})
+        elif not str.isalpha(str(reginfo['fname'])):
+            errors.update({'fname': 'First name cannot have number or symbols'})
+        if len(reginfo['lname']) < 2:
+            errors.update({'lname': 'The last name field must be at least two characters'})
+        elif not str.isalpha(str(reginfo['lname'])):
+            errors.update({'lname': 'Last name cannot have numbers or symbols'})
+        if not EMAILREGEX.match(reginfo['email']):
+            errors.update({'email': 'The E-Mail must be a valid e-mail address'})
+        if len(reginfo['password']) < 8:
+            errors.update({'password': 'Password must be at least 8 characters'})
+        elif not any(char.isdigit() for char in str(reginfo['password'])):
+            errors.update({'password': 'Password must contain at least one number'})
+        elif not any(char.isupper() for char in str(reginfo['password'])):
+            errors.update({'password': 'Password must contain at least one uppercase letter'})
+        if reginfo['confirmpass'] != reginfo['password']:
+            errors.update({'confirmpass': 'The password confirmation does not match the password'})
+        if len(errors) > 0:
+            return errors
+        else:
+            query1 = "SELECT email FROM users WHERE email = :email"
+            data1 = {"email": reginfo['email']}
+            if not self.db.query_db(query1, data1):
+                pw_hash = self.bcrypt.generate_password_hash(reginfo['password'])
+                query = "INSERT INTO users(first_name, last_name, email, password, created_on, updated_on) VALUES(:first_name, :last_name, :email, :password, now(), now())"
+                info = {
+                "first_name": reginfo['fname'],
+                "last_name": reginfo['lname'],
+                "email": reginfo['email'],
+                "password": pw_hash
+                }
+                self.db.query_db(query, info)
+                return "registered"
+            else:
+                errors.update({'user_registered': 'This E-Mail is already registered'})
+                return errors
+
+    def login_user(self, loginfo):
+        errors = {}
+        if not EMAILREGEX.match(loginfo['email']):
+            errors.update({'email2': 'The E-Mail must be a valid e-mail address'})
+        if len(loginfo['password']) < 8:
+            errors.update({'password2': 'Password must be at least 8 characters'})
+        if len(errors) > 0:
+            return errors
+        else:
+            query = "SELECT * FROM users WHERE email = :email LIMIT 1"
+            data = {'email': loginfo['email']}
+            user = self.db.query_db(query, data)
+            if user == []:
+                errors.update({'notreg': 'E-Mail is not registered.'})
+                return errors
+            else:
+                if self.bcrypt.check_password_hash(user[0]['password'], loginfo['password']):
+                    logged_info = {'logged_info':{'id': user[0]['id'], 'first_name': user[0]['first_name'], 'last_name': user[0]['last_name'], 'access': user[0]['access']}}
+                    return logged_info
+                else:
+                    errors.update({'passmatch': 'Incorrect password entered for registered E-Mail.'})
+                    return errors
+
+    def get_all_users(self):
+        query = "SELECT id as user_id, first_name, last_name, email, access, created_on FROM users"
+        return self.db.query_db(query)
+
+    def get_user(self, id):
+        query = "SELECT id as user_id, first_name, last_name, email, description, access, created_on FROM users WHERE id = :id"
+        data = {'id': id}
+        return self.db.query_db(query, data)
+
+    def update_user_info(self, user_info):
+        query = "UPDATE users SET first_name = :fname, last_name = :lname, email = :email, updated_on = now() WHERE id = :id"
+        data = {
+                'fname': user_info['fname'],
+                'lname': user_info['lname'],
+                'email': user_info['email'],
+                'id': user_info['id'],
+                }
+        self.db.query_db(query, data)
+        return user_info['id']
+
+    def admin_update_user_info(self, user_info):
+        query = "UPDATE users SET first_name = :fname, last_name = :lname, email = :email, access = :access, updated_on = now() WHERE id = :id"
+        data = {
+                'fname': user_info['fname'],
+                'lname': user_info['lname'],
+                'email': user_info['email'],
+                'id': user_info['id'],
+                'access': user_info['access']
+                }
+        self.db.query_db(query, data)
+        return user_info['id']
+
+    def update_user_description(self, user_info):
+        query = "UPDATE users SET description = :description, updated_on = now() WHERE id = :user_id"
+        data = {
+                'description': user_info['description'],
+                'user_id': user_info['id'],
+                }
+        self.db.query_db(query, data)
+        return user_info['id']
+
+    def update_user_password(self, user_info):
+        errors = {}
+        if len(user_info['password']) < 8:
+            errors.update({'password': 'Password must be at least 8 characters'})
+        elif not any(char.isdigit() for char in str(user_info['password'])):
+            errors.update({'password': 'Password must contain at least one number'})
+        elif not any(char.isupper() for char in str(user_info['password'])):
+            errors.update({'password': 'Password must contain at least one uppercase letter'})
+        if user_info['confirmpass'] != user_info['password']:
+            errors.update({'confirmpass': 'The password confirmation does not match the password'})
+        if len(errors) > 0:
+            errors.update({'user_id': user_info['id']})
+            return errors
+        else:
+            pw_hash = self.bcrypt.generate_password_hash(user_info['password'])
+            query = "UPDATE users SET password = :password, updated_on = now() WHERE id = :user_id"
+            data = {
+                    'password': pw_hash,
+                    'user_id': user_info['id'],
+                    }
+            self.db.query_db(query, data)
+            return {'id': user_info['id']}
+    def remove_user(self, id):
+        query = 'DELETE FROM users WHERE id = :id'
+        data = {'id': id}
+        return self.db.query_db(query, data)
+
+    def get_messages(self, id):
+        query = 'SELECT messages.id as m_id, message, messages.created_on as m_created_on, wall_id, users.first_name, users.last_name FROM dashboard.messages JOIN users ON m_author_id = users.id WHERE wall_id = :id ORDER BY m_created_on DESC'
+        data = {'id': id}
+        return self.db.query_db(query, data)
+
+    def add_message(self, info):
+        query = 'INSERT INTO messages(message, created_on, updated_on, m_author_id, wall_id) VALUES(:message, now(), now(), :m_author_id, :wall_id)'
+        data = {
+                'message': info['message'],
+                'm_author_id': info['m_author_id'],
+                'wall_id': info['wall_id']
+                }
+        self.db.query_db(query, data)                
+        return info['wall_id']
+
+    def get_comments(self, id):
+        query = 'SELECT comments.id as c_id, comment, comments.created_on as c_created_on, c_message_id, wall_id, users.first_name, users.last_name FROM comments JOIN users on c_author_id = users.id WHERE wall_id = :id ORDER BY comments.created_on DESC'
+        data = {'id': id}
+        return self.db.query_db(query, data)
+
+    def add_comment(self, info, id):
+        query = 'INSERT INTO comments(comment, created_on, updated_on, c_author_id, c_message_id, wall_id) VALUES(:comment, now(), now(), :c_author_id, :c_message_id, :wall_id)'
+        data = {
+                'comment': info['comment'],
+                'c_author_id': info['c_author_id'],
+                'c_message_id': id,
+                'wall_id': info['wall_id']
+                }
+        self.db.query_db(query, data)        
+        return info['wall_id']
